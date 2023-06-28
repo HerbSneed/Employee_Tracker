9da44895e962a3ac7aa2046c13dad87354b39e1b
Herb Sneed <hmsneed@gmail.com>
2023-06-27 21:19:45 -0400

    adding comments

--- Develop/public/index.js
+++ Develop/public/index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const db = require('../db');
 
+//Initial Prompt
 const initialPrompt = [
   {
     type: 'list',
@@ -19,8 +20,10 @@ const initialPrompt = [
   },
 ];
 
+// Init Function to start app
 function init() {
   inquirer.prompt(initialPrompt).then(response => {
+    // If statement displays department table query
     if (response.initialPrompt === 'View All Departments') {
       db.query(
         'SELECT DISTINCT department.id, department.department FROM department',
@@ -29,15 +32,17 @@ function init() {
           return init();
         }
       );
+      // Else if statement displays role table query
     } else if (response.initialPrompt === 'View All Roles') {
       db.query(
         'SELECT DISTINCT role.id, role.title, role.salary, department.department AS department FROM role JOIN department ON role.department_id = department.id',
-        
+
         function (err, results) {
           console.table(results);
           return init();
         }
       );
+      // Else if statement displays employee_tracker table query that joins role and department tables
     } else if (response.initialPrompt === 'View All Employees') {
       db.query(
         'SELECT DISTINCT employee_tracker.id, employee_tracker.first_name, employee_tracker.last_name, role.title, role.salary, department.department AS department, employee_tracker.manager FROM employee_tracker JOIN role ON employee_tracker.role_id = role.id JOIN department ON role.department_id = department.id',
@@ -46,6 +51,7 @@ function init() {
           return init();
         }
       );
+      // Else if statement that prompts addDepartment
     } else if (response.initialPrompt === 'Add Department') {
       const addDepartment = [
         {
@@ -55,6 +61,7 @@ function init() {
         },
       ];
       inquirer.prompt(addDepartment).then(answer => {
+        // Inserts user response into department table
         const departmentName = answer.addDepartment;
         db.query(
           'INSERT INTO department (department) VALUES (?)',
@@ -65,10 +72,11 @@ function init() {
               return;
             }
             console.log(`Added ${departmentName} to the Database`);
-          return init();
+            return init();
           }
         );
       });
+      // Else if statement that prompts addRole
     } else if (response.initialPrompt === 'Add Role') {
       const addRole = [
         {
@@ -88,7 +96,7 @@ function init() {
           name: 'department',
         },
       ];
-
+      // selects data from department table to populate department.list array
       db.query('SELECT * FROM department', function (err, departmentResults) {
         if (err) {
           console.error(err);
@@ -104,6 +112,7 @@ function init() {
           ).id;
           const roleTitle = answer.role;
           const roleSalary = answer.salary;
+          // inserts title, salary and departmentId into role table
           db.query(
             'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)',
             [roleTitle, roleSalary, departmentId],
@@ -118,6 +127,7 @@ function init() {
           );
         });
       });
+      // Else if statement that prompts addEmployee
     } else if (response.initialPrompt === 'Add an Employee') {
       const addEmployee = [
         {
@@ -143,14 +153,16 @@ function init() {
           name: 'employeeManager',
         },
       ];
-
+      // selects data from role table to populate addEmployee.employeeRole array
       db.query('SELECT * FROM role', function (err, employeeRoles) {
         if (err) {
           console.error(err);
           return;
         }
         addEmployee[2].choices = employeeRoles.map(role => role.title);
-
+        /* selects id, first and last name from employee_tracker table, combines them under the column full_name. 
+        This is user to populate addEmployee.employeeManager array. 
+        */
         db.query(
           'SELECT id, CONCAT(first_name, " ", last_name) AS full_name FROM employee_tracker',
           function (err, managerResults) {
@@ -178,7 +190,7 @@ function init() {
                   : managerResults.find(
                       manager => manager.full_name === managerName
                     ).id;
-
+              // inserts user response into the employee_tracker table
               db.query(
                 'INSERT INTO employee_tracker (first_name, last_name, role_id, manager_id, manager) VALUES (?, ?, ?, ?, ?)',
                 [
@@ -203,6 +215,7 @@ function init() {
           }
         );
       });
+      // Else if statement that prompts updateEmployeeRole
     } else if (response.initialPrompt === 'Update Employee Role') {
       const updateEmployeeRole = [
         {
@@ -218,6 +231,8 @@ function init() {
           name: 'employeeAssignedRole',
         },
       ];
+      /* selects id, first and last name from the employee_tracker table, combines them under the column full_name. 
+      This is used to populate updateEmployeerole.employeeRoleUpdate array. */
       db.query(
         'SELECT id, CONCAT(first_name, " ", last_name) AS full_name FROM employee_tracker',
         function (err, employeeResults) {
@@ -229,6 +244,7 @@ function init() {
           updateEmployeeRole[0].choices = employeeResults.map(
             employee => employee.full_name
           );
+          // selects data from role table, and populates updateEmployeerole.employeeAssignedRole array.
 
           db.query('SELECT * FROM role', function (err, roleResults) {
             if (err) {
@@ -247,6 +263,8 @@ function init() {
               );
               const roleId = role.id;
               const employeeId = employee.id;
+
+              // Updates the employee_tracker table with choosen new role.
               db.query(
                 'UPDATE employee_tracker SET role_id = ? WHERE id = ?',
                 [roleId, employeeId],
@@ -256,16 +274,17 @@ function init() {
                     return;
                   }
                   console.log(`Updated ${employeeName}'s role in the Database`);
-                return init();
+                  return init();
                 }
               );
             });
           });
         }
       );
+      // else if statement that quits the applicaiton.
     } else if (response.initialPrompt === 'Quit') {
-        console.log('Exiting the application...');
-        process.exit(); 
+      console.log('Exiting the application...');
+      process.exit();
     }
   });
 }
--- Develop/server.js
+++ Develop/server.js
@@ -1,35 +1,11 @@
 const express = require('express');
 const PORT = process.env.PORT || 3004;
 const app = express();
-const db = require('./db');
 const init = require('./public/index.js');
 
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.get('/api/department', (req, res) => {
-  const query = 'SELECT * from department';
-  db.query(query, (err, departments) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'server error' });
-    }
-    res.json(departments);
-  });
-});
-
-app.get('/api/role', (req, res) => {
-  db.query('select * from role', function (err, role) {
-    res.json(role);
-  });
-});
-
-app.get('/api/employee_tracker', (req, res) => {
-  db.query('select * from employee_tracker', function (err, employee_tracker) {
-    console.log(employee_tracker);
-  });
-});
 
 app.get('/init', (req, res) => {
   init(); 
